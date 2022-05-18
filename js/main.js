58112c958bfb31ef88aabd4bcd1980e011c8d32e
lucasg78 <lucasgallo@gmail.com>
2022-05-18 16:42:01 -0300

    Actualizando

--- js/main.js
+++ js/main.js
@@ -73,27 +73,24 @@ if (document.querySelector("#formNuevoCliente")) {
 
 const header = `<h2 class="mt-4">Saldos</h2>
   <div class="row mb-3">
-    <div class="col-md-3 themed-grid-col">Cuenta corriente<br>$ ${
-      cliente.hasOwnProperty("saldo")
-        ? new Intl.NumberFormat("es-ES").format(
-            parseFloat(cliente.saldo.CC).toFixed(2)
-          )
-        : ""
-    }</div>
-    <div class="col-md-3 themed-grid-col">Caja de ahorro en pesos<br>$ ${
-      cliente.hasOwnProperty("saldo")
-        ? new Intl.NumberFormat("es-ES").format(
-            parseFloat(cliente.saldo.CP).toFixed(2)
-          )
-        : ""
-    }</div>
-    <div class="col-md-3 themed-grid-col">Caja de ahorro en dólares<br>U$S ${
-      cliente.hasOwnProperty("saldo")
-        ? new Intl.NumberFormat("es-ES").format(
-            parseFloat(cliente.saldo.CD).toFixed(2)
-          )
-        : ""
-    }</div>
+    <div class="col-md-3 themed-grid-col">Cuenta corriente<br>$ ${cliente.hasOwnProperty("saldo")
+    ? new Intl.NumberFormat("es-ES").format(
+      parseFloat(cliente.saldo.CC).toFixed(2)
+    )
+    : ""
+  }</div>
+    <div class="col-md-3 themed-grid-col">Caja de ahorro en pesos<br>$ ${cliente.hasOwnProperty("saldo")
+    ? new Intl.NumberFormat("es-ES").format(
+      parseFloat(cliente.saldo.CP).toFixed(2)
+    )
+    : ""
+  }</div>
+    <div class="col-md-3 themed-grid-col">Caja de ahorro en dólares<br>U$S ${cliente.hasOwnProperty("saldo")
+    ? new Intl.NumberFormat("es-ES").format(
+      parseFloat(cliente.saldo.CD).toFixed(2)
+    )
+    : ""
+  }</div>
   </div><hr>`;
 
 function nuevoCliente(e) {
@@ -592,8 +589,8 @@ function abrirMenuOp(menu) {
           `<h2 class="dolarTitulo0">Compra de dólares</h2>
                                                                     <h4 class="normativaBCRA">Le recordamos que la operatoria de compra de dólares se encuentra regulada por la normativa de Exterior y Cambios del BCRA y la Ley Penal Cambiarla. La compra es sólo para atesoramiento personal. <strong>El cupo de U$S 200 es mensual y por persona.</strong> Se encuentra prohibido tanto ceder y/o vender el cupo mensual. No se puede comprar dólares a favor de o por cuentas de terceros. En el caso de que se detecte la violación a la normativa vigente, el banco se reserva el derecho de cerrar las cuentas, efectuar la correspondiente denuncia al BCRA y tomar cualquier medida que estime necesaria.</h4>                               
                                                                         <h3 class="dolarTitulo1">Comprás a $${new Intl.NumberFormat().format(
-                                                                          data
-                                                                        )} sin impuestos ni retenciones</h3>
+            data
+          )} sin impuestos ni retenciones</h3>
                                                                         <h4 class="cotizacionDolar">Cotización dólar por unidad en el Mercado Libre de Cambios, ámbito de aplicación y vigencia para operaciones por banca online al momento de su consulta</h4>
                                                                     </h4>                               
                                                                     <h4 class="ingresarImporte">Ingresá el importe en U$S</h4>
@@ -616,9 +613,8 @@ function abrirMenuOp(menu) {
               );
               console.log(precioDolar);
               const costoOperacion = montoDolar * precioDolar * 1.65;
-              document.querySelector("#costoOperacion").textContent = `${
-                Math.round(costoOperacion * 100) / 100
-              }`;
+              document.querySelector("#costoOperacion").textContent = `${Math.round(costoOperacion * 100) / 100
+                }`;
             });
         }
       });
@@ -700,22 +696,22 @@ function abrirMenuOp(menu) {
     element.remove();
     let content =
       header +
-      `<table>
+      `<table class="table table-striped">
       <thead>
-          <th>Numero de transaccion</th>
-          <th>tipo de transaccion</th>
-          <th>monto de transaccion</th>
-          <th>origen</th>
-          <th>destino</th>
+          <th class="tituloColumna">Transacción</th>
+          <th class="tituloColumna">Tipo</th>
+          <th class="tituloColumna">Monto</th>
+          <th class="tituloColumna">Origen</th>
+          <th class="tituloColumna">Destino</th>
       </thead>
       <tbody>`;
     cliente.operaciones.map((op, index) => {
       content += `<tr>
-      <td>${index + 1}</td>
-                    <td>${op.tipo}</td>
-                    <td>${op.importe}</td>
-                    <td>${op.origen}</td>
-                    <td>${op.destino}</td></tr>
+                    <td class="celda">${index + 1}</td>
+                    <td class="celda">${op.tipo}</td>
+                    <td class="montoResumen">${new Intl.NumberFormat().format(op.importe)}</td>
+                    <td class="celda">${op.origen}</td>
+                    <td class="celda">${op.destino}</td></tr>
         `;
     });
     content += "</table></table>";
